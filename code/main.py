1ebf9a64ca8d976206c43521d62e35a58ebeaaf8
skipper H <h.skipper.work@gmail.com>
2024-03-15 20:38:50 +0530

    created a Jason file and displayed a task name

--- code/main.py
+++ code/main.py
@@ -1,15 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
-import time
+from datetime import datetime
+from json import load
 
 def main():
+
+    Current_task = 0
+
+    #* open tasks.json --------------------------------
+    with open(r"tasks.json", "r") as file:
+        tasks = load(file)
+        print(tasks)
+
     #* define screen --------------------------------
     root =  tk.Tk()
     root.configure(padx=32,pady=8)
     
+    #* define frame --------------------------------
     bottom_frame = ttk.Frame(root, name="time loging").grid(column=0,row=1)
-    ttk.Label(bottom_frame, text="hello world").grid(column=1,row=0,padx=10,pady=10)
+    ttk.Label(bottom_frame, text=(list(tasks.keys())[0])).grid(column=1,row=0,padx=10,pady=10)
+
     root.mainloop()
     
+    #todo: Ability to Create a new task 
+    #todo: Ability to Log time
+
 if __name__ == '__main__':
     main()
